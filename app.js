2e6b185342aaff491662e669a0f0254f116f9eb2
nosnickers <nosnickers@gmail.com>
2017-09-24 19:20:34 +0800

    change port 8080

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-app.listen(3000, function () {
-  console.log('app is listening at port 3000');
+app.listen(8080, function () {
+  console.log('app is listening at port 8080');
 });
